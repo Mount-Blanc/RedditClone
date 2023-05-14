eaf7d0b688c5eaca40152f0175b6ae54a204e213
Mount-Blanc <mountblanc100@gmail.com>
2023-05-14 10:14:28 -0500

    added proper flex css

--- RedditClone/src/App.jsx
+++ RedditClone/src/App.jsx
@@ -1,13 +1,17 @@
-
+import './App.css'
 
 function App() {
 
   return (
     <>
     <header>
+      <div className='header-container'>
+        <div className='nav-left'>
       <div>logo</div>
       <div>Search</div>
+      </div>
       <div>Login</div>
+      </div>
     </header>
     <main>
       <div>Side panel</div>
