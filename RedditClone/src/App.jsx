afc450f18e9c7e702ed7a9874df0f7f83bd8bb85
Mount-Blanc <mountblanc100@gmail.com>
2023-05-14 10:33:32 -0500

    added basic elements of side drawer

--- RedditClone/src/App.jsx
+++ RedditClone/src/App.jsx
@@ -8,13 +8,49 @@ function App() {
       <div className='header-container'>
         <div className='nav-left'>
       <div>logo</div>
-      <div>Search</div>
+      <div >
+        <form>
+          <input className='header-searchbar' placeholder='Search Reddit'/>
+        </form>
+      </div>
       </div>
       <div>Login</div>
       </div>
     </header>
     <main>
-      <div>Side panel</div>
+      <div className='side-drawer'>Side panel
+      <button>
+        <span>
+          <span>Home</span>
+          </span>
+          </button>
+          <button>
+        <span>
+          <span>Popular</span>
+          </span>
+          </button>
+          <button>
+        <span>
+          <span>Gaming</span>
+          </span>
+          </button>
+          <button>
+        <span>
+          <span>Sports</span>
+          </span>
+          </button>
+          <button>
+        <span>
+          <span>Business,Economics</span>
+          </span>
+          </button>
+          <button>
+        <span>
+          <span>Crypto</span>
+          </span>
+          </button> 
+                   </div>
+
     </main>
     </>
   )
